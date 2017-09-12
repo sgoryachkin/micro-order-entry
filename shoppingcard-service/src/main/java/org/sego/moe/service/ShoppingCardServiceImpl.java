4efecdecb11f5ffd1d1be70748b3c8f2d871793d
sego <sego@thinkprime>
2017-09-13 00:07:44 +0300

    fix

--- shoppingcard-service/src/main/java/org/sego/moe/service/ShoppingCardServiceImpl.java
+++ shoppingcard-service/src/main/java/org/sego/moe/service/ShoppingCardServiceImpl.java
@@ -1,8 +1,5 @@
 package org.sego.moe.service;
 
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 import org.sego.moe.commons.model.CardEvent;
 import org.sego.moe.dao.SalesOrderMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +14,8 @@ public class ShoppingCardServiceImpl implements ShoppingCardService {
 	@Autowired
 	SalesOrderMessageRepository messageRepository;
 	
-	List<CardEvent> eventStore = new CopyOnWriteArrayList<>();
-	
     public void addCartEvent(CardEvent cartEvent) {
     	messageRepository.save(cartEvent);
-    	//eventStore.add(cartEvent);
-    	System.out.println(cartEvent + "storeSize: " + eventStore.size());
     	cardOutputEventSource.sendMessage(cartEvent);
     }
 
